3a99521e0449526bf29005e850d541fd74b04aa3
JustMeRuby <104598428+JustMeRuby@users.noreply.github.com>
2024-06-24 04:57:34 +0700

    Add files via upload

---
+++ SmartphoneManagement/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace SmartphoneManagement
+{
+    partial class FrmPhoneManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPhoneManagement));
+            this.btnLoadExcel = new System.Windows.Forms.Button();
+            this.btnLoadSQL = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdateSource = new System.Windows.Forms.Button();
+            this.dgwPhoneList = new System.Windows.Forms.DataGridView();
+            this.picPhoneImage = new System.Windows.Forms.PictureBox();
+            this.colSmartPhoneID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSmartPhoneName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSmartPhoneType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAnnouncedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPlatform = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCamera = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRAM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colBattery = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPhoneList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoneImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadExcel
+            // 
+            this.btnLoadExcel.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadExcel.Name = "btnLoadExcel";
+            this.btnLoadExcel.Size = new System.Drawing.Size(168, 23);
+            this.btnLoadExcel.TabIndex = 0;
+            this.btnLoadExcel.Text = "Load Data From Excel";
+            this.btnLoadExcel.UseVisualStyleBackColor = true;
+            this.btnLoadExcel.Click += new System.EventHandler(this.btnLoadExcel_Click);
+            // 
+            // btnLoadSQL
+            // 
+            this.btnLoadSQL.Location = new System.Drawing.Point(208, 12);
+            this.btnLoadSQL.Name = "btnLoadSQL";
+            this.btnLoadSQL.Size = new System.Drawing.Size(168, 23);
+            this.btnLoadSQL.TabIndex = 1;
+            this.btnLoadSQL.Text = "Load Data From SQL";
+            this.btnLoadSQL.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(36, 305);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(117, 305);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(198, 305);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateSource
+            // 
+            this.btnUpdateSource.Location = new System.Drawing.Point(330, 305);
+            this.btnUpdateSource.Name = "btnUpdateSource";
+            this.btnUpdateSource.Size = new System.Drawing.Size(125, 23);
+            this.btnUpdateSource.TabIndex = 5;
+            this.btnUpdateSource.Text = "Update to DataSource";
+            this.btnUpdateSource.UseVisualStyleBackColor = true;
+            // 
+            // dgwPhoneList
+            // 
+            this.dgwPhoneList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwPhoneList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colSmartPhoneID,
+            this.colSmartPhoneName,
+            this.colSmartPhoneType,
+            this.colAnnouncedDate,
+            this.colPlatform,
+            this.colCamera,
+            this.colRAM,
+            this.colBattery,
+            this.colPrice});
+            this.dgwPhoneList.Location = new System.Drawing.Point(12, 44);
+            this.dgwPhoneList.MultiSelect = false;
+            this.dgwPhoneList.Name = "dgwPhoneList";
+            this.dgwPhoneList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgwPhoneList.Size = new System.Drawing.Size(443, 255);
+            this.dgwPhoneList.TabIndex = 6;
+            this.dgwPhoneList.SelectionChanged += new System.EventHandler(this.dgwPhoneList_SelectionChanged);
+            // 
+            // picPhoneImage
+            // 
+            this.picPhoneImage.Image = ((System.Drawing.Image)(resources.GetObject("picPhoneImage.Image")));
+            this.picPhoneImage.Location = new System.Drawing.Point(461, 103);
+            this.picPhoneImage.Name = "picPhoneImage";
+            this.picPhoneImage.Size = new System.Drawing.Size(205, 144);
+            this.picPhoneImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPhoneImage.TabIndex = 7;
+            this.picPhoneImage.TabStop = false;
+            // 
+            // colSmartPhoneID
+            // 
+            this.colSmartPhoneID.DataPropertyName = "SmartPhoneID";
+            this.colSmartPhoneID.HeaderText = "SmartPhoneID";
+            this.colSmartPhoneID.Name = "colSmartPhoneID";
+            // 
+            // colSmartPhoneName
+            // 
+            this.colSmartPhoneName.DataPropertyName = "SmartPhoneName";
+            this.colSmartPhoneName.HeaderText = "SmartPhone Name";
+            this.colSmartPhoneName.Name = "colSmartPhoneName";
+            // 
+            // colSmartPhoneType
+            // 
+            this.colSmartPhoneType.DataPropertyName = "SmartPhoneType";
+            this.colSmartPhoneType.HeaderText = "SmartPhone Type";
+            this.colSmartPhoneType.Name = "colSmartPhoneType";
+            // 
+            // colAnnouncedDate
+            // 
+            this.colAnnouncedDate.DataPropertyName = "AnnouncedDate";
+            this.colAnnouncedDate.HeaderText = "Announced Date";
+            this.colAnnouncedDate.Name = "colAnnouncedDate";
+            // 
+            // colPlatform
+            // 
+            this.colPlatform.DataPropertyName = "Platform";
+            this.colPlatform.HeaderText = "Platform";
+            this.colPlatform.Name = "colPlatform";
+            // 
+            // colCamera
+            // 
+            this.colCamera.DataPropertyName = "Camera";
+            this.colCamera.HeaderText = "Camera";
+            this.colCamera.Name = "colCamera";
+            // 
+            // colRAM
+            // 
+            this.colRAM.DataPropertyName = "RAM";
+            this.colRAM.HeaderText = "RAM";
+            this.colRAM.Name = "colRAM";
+            // 
+            // colBattery
+            // 
+            this.colBattery.DataPropertyName = "Battery";
+            this.colBattery.HeaderText = "Battery";
+            this.colBattery.Name = "colBattery";
+            // 
+            // colPrice
+            // 
+            this.colPrice.DataPropertyName = "Price";
+            this.colPrice.HeaderText = "Price";
+            this.colPrice.Name = "colPrice";
+            // 
+            // FrmPhoneManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 337);
+            this.Controls.Add(this.picPhoneImage);
+            this.Controls.Add(this.dgwPhoneList);
+            this.Controls.Add(this.btnUpdateSource);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLoadSQL);
+            this.Controls.Add(this.btnLoadExcel);
+            this.Name = "FrmPhoneManagement";
+            this.Text = "Phone Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPhoneList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoneImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadExcel;
+        private System.Windows.Forms.Button btnLoadSQL;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdateSource;
+        private System.Windows.Forms.DataGridView dgwPhoneList;
+        private System.Windows.Forms.PictureBox picPhoneImage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSmartPhoneID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSmartPhoneName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSmartPhoneType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAnnouncedDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPlatform;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCamera;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRAM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colBattery;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+    }
+}
+
---
+++ SmartphoneManagement/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Globalization;
+using System.Data.SqlClient;
+
+namespace SmartphoneManagement
+{
+    public partial class FrmPhoneManagement : System.Windows.Forms.Form
+    {
+        public List<SmartPhone> SPList = new List<SmartPhone>();
+        // loadData = 0 (chưa có dữ liệu)
+        // loadData = 1 (chưa có dữ liệu từ excel)
+        // loadData = 2 (chưa có dữ liệu từ sql)
+        public int loadData = 0;
+        static string ProjectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        string ExcelFilaPath = ProjectPath + "\\Data\\SmartPhoneList.xlsx";
+        //string connectionString = "Data Source = ; Initial Catalog = SmartPhoneID; Intergrated Security = SSPI";
+        int CurrentPhoneIndex = -1;
+        DataTable datatable;
+        BindingSource binding = new BindingSource();
+        
+        public int ReadDataFromFile(List<SmartPhone> DataList, string FilePath, int colCount)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(FilePath);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            xlWorksheet.Columns.ClearFormats();
+            xlWorksheet.Rows.ClearFormats();
+
+            int rowCount = xlWorksheet.UsedRange.Rows.Count;
+
+            int numPhone = 0;
+            string SmartPhoneID = "";
+            string SmartPhoneName = "";
+            string SmartPhoneType = "";
+            DateTime AnnouncedDate = DateTime.Now;
+            string Platform = "";
+            string Camera = "";
+            string RAM = "";
+            string Battery = "";
+            int Price = 0;
+            string Avatar = "";
+
+            for (int i = 2; i <= colCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    switch (j)
+                    {
+                        case 1:
+                            SmartPhoneID = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 2:
+                            SmartPhoneName = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 3:
+                            SmartPhoneType = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 4:
+                            AnnouncedDate = DateTime.ParseExact(xlRange.Cells[i, j].Value2.ToString(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                            break;
+                        case 5:
+                            Platform = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 6:
+                            Camera = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 7:
+                            RAM = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 8:
+                            Battery = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                        case 9:
+                            Price = Convert.ToInt32(xlRange.Cells[i, j].Value2.ToString());
+                            break;
+                        case 10:
+                            SmartPhoneID = xlRange.Cells[i, j].Value2.ToString();
+                            break;
+                    }
+                }
+                DataList.Add(new SmartPhone());
+                DataList[numPhone].SmartPhoneID = SmartPhoneID;
+                DataList[numPhone].SmartPhoneName = SmartPhoneName;
+                DataList[numPhone].SmartPhoneType = SmartPhoneType;
+                DataList[numPhone].AnnouncedDate = AnnouncedDate;
+                DataList[numPhone].Platform = Platform;
+                DataList[numPhone].Camera = Camera;
+                DataList[numPhone].RAM = RAM;
+                DataList[numPhone].Battery = Battery;
+                DataList[numPhone].Price = Price;
+                DataList[numPhone].Avatar = Avatar;
+                numPhone = numPhone + 1;
+            }
+            xlApp.Quit();
+
+            MessageBox.Show("Load Data From Excel Finished! : " + (rowCount - 1).ToString() + " Records");
+
+            return (rowCount - 1); // Không tính dòng tiêu đề
+        }
+
+        public FrmPhoneManagement()
+        {
+            InitializeComponent();
+
+            var sublist = SPList.Select(x => new
+                                        {
+                                            SmartPhoneID = x.SmartPhoneID,
+                                            SmartPhoneName = x.SmartPhoneName,
+                                            SmartPhoneType = x.SmartPhoneType,
+                                            AnnouncedDate = x.AnnouncedDate.ToString("dd/MM/yyyy"),
+                                            Platform = x.Platform,
+                                            Camera = x.Camera,
+                                            RAM = x.RAM,
+                                            Battery = x.Battery,
+                                            Price = x.Price.ToString() + "USD",
+                                        }).ToList();
+
+            datatable.Columns.Add("SmartPhoneID");
+            datatable.Columns.Add("SmartPhoneName");
+            datatable.Columns.Add("SmartPhoneType");
+            datatable.Columns.Add("AnnouncedDate");
+            datatable.Columns.Add("Platform");
+            datatable.Columns.Add("Camera");
+            datatable.Columns.Add("RAM");
+            datatable.Columns.Add("Battery");
+            datatable.Columns.Add("Price");
+            
+
+            DataRow newrow;
+            foreach (var bi in sublist)
+            {
+                newrow = datatable.NewRow();
+                newrow["SmartPhoneID"] = bi.SmartPhoneID;
+                newrow["SmartPhoneName"] = bi.SmartPhoneName;
+                newrow["SmartPhoneType"] = bi.SmartPhoneType;
+                newrow["AnnouncedDate"] = bi.AnnouncedDate;
+                newrow["Platform"] = bi.Platform;
+                newrow["Camera"] = bi.Camera;
+                newrow["RAM"] = bi.RAM;
+                newrow["Battery"] = bi.Battery;
+                newrow["Price"] = bi.Price;
+                datatable.Rows.Add(newrow);
+                datatable.AcceptChanges();
+            }
+
+            binding.AllowNew = true;
+            binding.DataSource = datatable;
+            dgwPhoneList.AutoGenerateColumns = false;
+            dgwPhoneList.DataSource = binding;
+        }
+
+        private void dgwPhoneList_SelectionChanged(object sender, EventArgs e)
+        {
+            CurrentPhoneIndex = dgwPhoneList.CurrentRow.Index;
+            if (CurrentPhoneIndex >= 0 && CurrentPhoneIndex < SPList.Count)
+                picPhoneImage.Image = Image.FromFile(ProjectPath + "\\Data\\" + SPList[CurrentPhoneIndex].Avatar);
+        }
+
+        private void btnLoadExcel_Click(object sender, EventArgs e)
+        {
+            loadData = 1;
+            datatable = new DataTable();
+            SPList.Clear();
+
+            int colCount = 10;
+            int NumDataRow = ReadDataFromFile(SPList, ExcelFilaPath, colCount);
+
+            var sublist = SPList.Select(x => new
+                                        {
+                                            SmartPhoneID = x.SmartPhoneID,
+                                            SmartPhoneName = x.SmartPhoneName,
+                                            SmartPhoneType = x.SmartPhoneType,
+                                            AnnouncedDate = x.AnnouncedDate.ToString("dd/MM/yyyy"),
+                                            Platform = x.Platform,
+                                            Camera = x.Camera,
+                                            RAM = x.RAM,
+                                            Battery = x.Battery,
+                                            Price = x.Price.ToString() + "USD",
+                                        }).ToList();
+
+            binding.AllowNew = true;
+            binding.DataSource = datatable;
+            dgwPhoneList.AutoGenerateColumns = false;
+            dgwPhoneList.DataSource = binding;
+        }
+    }
+}
